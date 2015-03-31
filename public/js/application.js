2c2d96a702d5cf4d166857221f5de992f2e65db0
miniyakkos <ayakokurushima@gmail.com>
2015-03-30 19:39:49 -0700

    Add directions to map, simple CSS

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,50 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
 });
+var directionsDisplay;
+var directionsService;
+var map;
+var stepDisplay;
+
+
+function initialize() {
+  directionsService = new google.maps.DirectionsService();
+  var mapOptions = {
+    zoom: 12,
+    center: new google.maps.LatLng(37.7833, -122.4167)
+  };
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+  var rendererOptions = {
+    map: map
+  };
+  directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);
+  stepDisplay = new google.maps.InfoWindow();
+};
+
+  function loadScript() {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://maps.googleapis.com/maps/api/js?libraries=drawing' +
+        '&callback=initialize';
+    document.body.appendChild(script);
+  };
+
+
+window.onload = loadScript;
+
+function calcRoute() {
+  var start = document.getElementById("start").value;
+  var end = document.getElementById("end").value;
+  var request = {
+    origin: start,
+    destination: end,
+    travelMode: google.maps.TravelMode.WALKING
+  };
+  directionsService.route(request, function(result, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(result);
+    }
+  });
+};
+
