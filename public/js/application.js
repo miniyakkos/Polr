5cec851e2dfc45a5a129bb5b6d23dbf2aac767ba
miniyakkos <ayakokurushima@gmail.com>
2015-04-23 14:35:14 -0700

    Graph works when drawPath function called after jquery

--- public/js/application.js
+++ public/js/application.js
@@ -1,11 +1,18 @@
 $(document).ready(function() {
- initialize();
- $(".route").on("click", calcRoute)
+  initialize();
+  $(".route").on("click", function() {
+    calcRoute();
+    drawPath();
+  });
 });
 
 var directionsDisplay;
 var directionsService = new google.maps.DirectionsService();
 var map;
+var pointArray;
+var elevator = new google.maps.ElevationService();
+var chart;
+google.load("visualization", "1.0", {packages: ["columnchart"]});
 
 function initialize() {
   directionsDisplay = new google.maps.DirectionsRenderer();
@@ -16,7 +23,7 @@ function initialize() {
   map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
   directionsDisplay.setMap(map);
   directionsDisplay.setPanel(document.getElementById("directionsPanel"));
-}
+};
 
 function calcRoute() {
   var start = document.getElementById("origin").value;
@@ -29,8 +36,44 @@ function calcRoute() {
   directionsService.route(request, function(response, status) {
     if (status == google.maps.DirectionsStatus.OK) {
       directionsDisplay.setDirections(response);
+      pointArray = response.routes[0].overview_path
     }
   });
+  debugger
+};
+
+function drawPath() {
+  chart = new google.visualization.ColumnChart(document.getElementById('elevation_chart'));
+  debugger
+  var pathRequest = {
+    'path': pointArray,
+    'samples': 50
+  };
+    elevator.getElevationAlongPath(pathRequest, plotElevation);
+};
+
+function plotElevation(results, status) {
+  if (status == google.maps.ElevationStatus.OK) {
+    elevations = results;
+    var elevationPath = [];
+    for (var i = 0; i < results.length; i++) {
+      elevationPath.push(elevations[i].location);
+    }
+    var data = new google.visualization.DataTable();
+    data.addColumn('string', 'Sample');
+    data.addColumn('number', 'Elevation');
+    for (var i = 0; i < results.length; i++) {
+      data.addRow(['', elevations[i].elevation]);
+    }
+    document.getElementById('elevation_chart').style.display = 'block';
+    chart.draw(data, {
+      width: 640,
+      height: 200,
+      legend: 'none',
+      titleY: 'Elevation (m)'
+    });
+  }
 }
 
-
+// 2085 ala wai blvd honolulu hi 96815
+// 900 fort street mall honolulu hi 96813
